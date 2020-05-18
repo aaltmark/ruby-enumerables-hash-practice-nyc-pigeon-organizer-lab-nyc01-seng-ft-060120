72d4da5a5edb4689fa0f14b6bf659390051209bc
Adam Altmark <adamaltmark@gmail.com>
2020-05-18 14:24:29 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,6 +1,7 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
   final_hash = {}
-  data.each do |key|
+  data.each do |key, value|
     data[:gender][:male].inverse
     data[:gender][:female].inverse
   end
