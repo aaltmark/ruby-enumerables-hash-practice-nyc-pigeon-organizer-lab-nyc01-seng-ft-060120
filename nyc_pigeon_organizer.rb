724686cb29ac9913c9cb5adf33823be1a5eb6687
Adam Altmark <adamaltmark@gmail.com>
2020-05-18 14:35:36 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,6 +4,9 @@ def nyc_pigeon_organizer(data)
   data.each do |key, value|
   value.each do |new_value, names|
   names.each do |new_name|
+    if !final_hash[new_name]
+      final_hash[new_name] = {}
+    end
     binding.pry
   end
   end
